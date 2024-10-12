64b5ba62c127884391502ad916737e24edebd664
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-10-13 02:57:04 +0600

    Add Module 03 - Variables, Data Types, and Math Operators -> Lesson 02 - Variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int myVar;
+        myVar = 50;
+        System.out.println("myVar: " + myVar);
+
+        int anotherVar = 100;
+        System.out.println("anotherVar: " + anotherVar);
+
+        myVar = anotherVar;
+        System.out.println("myVar: " + myVar);
+
+        // once the value of a `final` variable is set, you cannot modify it.
+        final int maxStudent = 25;
+        System.out.println("maxStudent: " + maxStudent);
+
+        final int someVariable;
+        final int someOtherVariable = 100;
+        someVariable = someOtherVariable;
+        System.out.println("someVariable: " + someVariable);
+        System.out.println("someOtherVariable: " + someOtherVariable);
     }
 }
