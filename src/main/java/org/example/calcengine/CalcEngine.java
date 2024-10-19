5f9353fcafc957b6b1a87004a580a00f010ca2aa
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-10-20 03:21:34 +0600

    Add Module 04 - Conditional Logic and Block Statements -> Lesson 10 - Using Switch Statement and Conditional Assignment

--- src/main/java/org/example/calcengine/CalcEngine.java
+++ src/main/java/org/example/calcengine/CalcEngine.java
@@ -1,23 +1,30 @@
 package org.example.calcengine;
 
 public class CalcEngine {
+    // 10 - Using Switch Statement and Conditional Assignment
     public static void engine() {
         double val1 = 100.0d;
         double val2 = 50.0d;
         double result = 0.0d;
         char optCode = 'd';
 
-        if (optCode == 'a')
-            result = val1 + val2;
-        else if (optCode == 's')
-            result = val1 - val2;
-        else if (optCode == 'm')
-            result = val1 * val2;
-        else if (optCode == 'd')
-            result = val1 / val2;
-        else {
-            System.out.println("Invalid optCode: " + optCode);
-            result = 0.0d;
+
+        switch (optCode) {
+            case 'a':
+                result = val1 + val2;
+                break;
+            case 's':
+                result = val1 - val2;
+                break;
+            case 'm':
+                result = val1 * val2;
+                break;
+            case 'd':
+                result = val2 != 0 ? val1 / val2 : 0.0d;
+            default:
+                System.out.println("Invalid optCode: " + optCode);
+                result = 0.0d;
+                break;
         }
 
         System.out.println(result);
