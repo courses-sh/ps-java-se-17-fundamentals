2f3694f956b93933f75ef7f029515af511256ae7
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2025-01-02 03:25:32 +0600

    Add Module 06 - Understanding Methods -> Lesson 03 - Parameters (refactor)

--- src/main/java/org/example/methods/DeclaringAndCallingMethods.java
+++ src/main/java/org/example/methods/DeclaringAndCallingMethods.java
@@ -1,11 +1,13 @@
 package org.example.methods;
 
 public class DeclaringAndCallingMethods {
+    // 02 - Declaring and Calling Methods
     public static void doSomething() {
         System.out.println("Inside the method");
         System.out.println("Still inside");
     }
 
+    // 03 - Parameters
     public static void showSum(float x, float y, int count) {
         float sum = x + y;
         for (int i = 1; i <= count; i++) {
